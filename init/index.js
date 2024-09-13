6a77df55af74539f273e3ea20ed995d6dcc41af3
YadneshUmredkar@75 <yadneshumredkar75@gmail.com>
2024-09-13 08:35:09 +0530

    app.ja update

--- init/index.js
+++ init/index.js
@@ -5,9 +5,9 @@ const init = require("./data.js");
 main().then(res=>console.log("Connected DB"))
 .catch(err=>console.log(err));
 async function main(){
-    await mongoose.connect(process.env.API_CLOUD_MONGODB);
+    await mongoose.connect("mongodb://127.0.0.1:27017/app");
 }
-// "mongodb://127.0.0.1:27017/app"
+// 
 const initdata=async()=>{
     await Listing.deleteMany({});
    init.data=init.data.map((obj)=>({...obj,owner:"66d2fac3bc3b1997ab46a491"}))
